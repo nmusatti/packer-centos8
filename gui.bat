ef2c385e19d8d2aef2beb87d198fe958f111d280
Nicola Musatti <nicola.musatti@gmail.com>
2020-02-16 17:53:07 +0100

    Corrected the .bat script files

--- gui.bat
+++ gui.bat
@@ -1,3 +1,3 @@
-rmdir /s centos8-gui
-IF %ERRORLEVEL% EQU 0 packer.io build centos-gui.json
+rmdir /s /q centos8-gui
+IF %ERRORLEVEL% EQU 0 packer build centos-gui.json
 IF %ERRORLEVEL% EQU 0 vagrant box add -f --name centos8-gui centos8-gui/package.box
--- mate.bat
+++ mate.bat
@@ -1,3 +1,3 @@
-rmdir /s centos8-mate
-IF %ERRORLEVEL% EQU 0 packer.io build centos-mate.json
+rmdir /s /q centos8-mate
+IF %ERRORLEVEL% EQU 0 packer build centos-mate.json
 IF %ERRORLEVEL% EQU 0 vagrant box add -f --name centos8-mate centos8-mate/package.box
