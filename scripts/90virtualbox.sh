a8572dd0a6779d21b0062a10e0f240669f488dda
Christian Kniep <kniec@amazon.com>
2022-01-24 16:51:36 +0100

    enable service

--- scripts/90virtualbox.sh
+++ scripts/90virtualbox.sh
@@ -16,4 +16,4 @@ mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
 rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
-
+systemctl enable vboxadd.service
